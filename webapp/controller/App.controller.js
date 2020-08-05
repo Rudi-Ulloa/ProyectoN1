c48e393f54574b19376937915e5ad666386c15e5
rudi-ulloa <rudi.ulloa@gmail.com>
2020-08-05 02:01:51 +0000

    N2

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"ProyectoN1/SAPUI5/model/models"
 
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, models) {
 	"use strict";
 	return Controller.extend("ProyectoN1.SAPUI5.controller.App", {
 			onInit: function () {
-				
+				this.getView().setModel(models.createRecipient());
 				},
 
 		onShowHola: function () {
